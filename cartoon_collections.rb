43293c70e7b1aa4814f1ca0ecc495e231293f6d6
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-07 02:47:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,8 @@ def roll_call_dwarves(dwarf)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |planeteer| planteer.capitalize + "!"  }
 end
 
 def long_planeteer_calls# code an argument here
